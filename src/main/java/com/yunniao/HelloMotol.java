09bf1d29ea8dc199fd43629b421a9063295e07c0
maorunning <maorunning@yunniao.me>
2019-03-13 19:14:51 +0800

    æµè¯

--- src/main/java/com/yunniao/HelloMotol.java
+++ src/main/java/com/yunniao/HelloMotol.java
@@ -3,5 +3,6 @@ package com.yunniao;
 public class HelloMotol {
     public static void main(String[] args) {
         System.out.println("tag 1.0");
+        System.out.println("tag v4.0测试");
     }
 }
